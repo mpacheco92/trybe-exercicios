e2aaf7f67f6654dbd0c6835ec099ef0e96f3239b
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-10-25 21:24:50 -0300

    exercicio-1

---
+++ fundamentos/secao-3-java-script/dia-4/objects.js
@@ -0,0 +1,16 @@
+const reader = {
+    name: 'Julia',
+    lastName: 'Pessoa',
+    age: 21,
+    favoriteBooks: [
+      {
+        title: 'O Senhor dos Anéis - a Sociedade do Anel',
+        author: 'J. R. R. Tolkien',
+        publisher: 'Martins Fontes',
+      },
+    ],
+  };
+
+//   1 - Acesse as chaves name, lastName e title e exiba informações em um console.log() no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Senhor dos Anéis - a Sociedade do Anel’.”.
+
+console.log(`O livro favorito de ${reader.name} ${reader.lastName} se chama ${reader.favoriteBooks[0].title}.`);
