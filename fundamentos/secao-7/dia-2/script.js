e52e1a5b30cdb6fc4fd7d905b665efac1d01f2b2
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-11-30 13:09:55 -0300

    exercicio 2

--- fundamentos/secao-7/dia-2/script.js
+++ fundamentos/secao-7/dia-2/script.js
@@ -10,3 +10,17 @@ const peoples = [
 
   const oldPeoples = peoples.filter((people) => people.age >= 30);
 
+/////////////////////////////////////////////////////////////
+
+//   Exercício 2
+//   Considere o seguinte array de objetos que representa uma lista de jogadores de basquete:
+const players = [
+    { name: 'Michael Jordan', height: 198, team: 'Chicago Bulls' },
+    { name: 'LeBron James', height: 203, team: 'Los Angeles Lakers' },
+    { name: 'Kobe Bryant', height: 198, team: 'Los Angeles Lakers' },
+    { name: 'Stephen Curry', height: 191, team: 'Golden State Warriors' },
+    { name: 'Kevin Durant', height: 211, team: 'Brooklyn Nets' },
+  ];
+//   Utilizando o método filter e array destructuring, crie uma função filterHighPlayers que retorna um novo array contendo apenas os jogadores com altura igual ou superior a 200 centímetros.
+
+const filterHighPlayers = () => players.filter((player) => player.height >= 200);
