f1fb9bf84b29e3991424ec34a46927de8da91dbd
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-01 14:28:48 -0300

    exercicio2

--- fundamentos/secao-7/dia-3/exercicios.js
+++ fundamentos/secao-7/dia-3/exercicios.js
@@ -3,6 +3,19 @@ const countries = require('./countriesData');
 // Exercício 1
 // Adicione a propriedade planet com o valor Earth em todos os países diretamente no array countries.
 
-const addPlanet = countries.forEach((country) => {
-    country.planet = 'Earth';
+countries.forEach((country) => country.planet = 'Earth');
+
+
+// Exercício 2
+// Para cada país, crie um objeto apenas com as informação do nome, código da moeda e nome da moeda. Essas informações devem ser armazenadas em um novo array. Faça esse exercício usando o método forEach, não vale map.
+
+const currencyArray = [];
+
+countries.forEach((country) => {
+  const currencyObj = {
+    name: country.name,
+    currencyCode: country.currencies[0].code,
+    currencyName: country.currencies[0].name,
+  };
+  currencyArray.push(currencyObj);
 });
