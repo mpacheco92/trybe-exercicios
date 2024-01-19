7c0ff734e22f4bf7d3eb9e2b3fadc96af5a6c888
Mylher Pacheco <mylherpacheco90@gmail.com>
2024-01-18 21:49:48 -0300

    HelloWorld finalizado

---
+++ front-end/secao-2/dia-1/hello-world/src/components/moduledetails.tsx
@@ -0,0 +1,12 @@
+export default function ModuleDetails() {
+  return (
+    <>
+      <p>✅ Fundamentos</p>
+      <p>⏳ Front-end</p>
+      <p>⏳ Back-end</p>
+      <p>⏳ Ciência da Computação</p>
+      <p>⏳ Soft Skills</p>
+      <p>⏳ Carreira</p>
+    </>
+  )
+}
